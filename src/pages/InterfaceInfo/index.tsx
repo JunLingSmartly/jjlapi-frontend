661a99977cd1da7a0d381aea9f0ac809336fa341
jinjunling <jinjunling@ultrapower.com.cn>
2024-01-26 11:03:51 +0800

    开发在线调用页面

--- src/pages/InterfaceInfo/index.tsx
+++ src/pages/InterfaceInfo/index.tsx
@@ -1,6 +1,10 @@
-import { getInterfaceInfoByIdUsingGet } from '@/services/jjlapi-backend/interfaceInfoController';
+import {
+  getInterfaceInfoByIdUsingGet,
+  invokeInterfaceInfoUsingPost,
+} from '@/services/jjlapi-backend/interfaceInfoController';
 import { PageContainer } from '@ant-design/pro-components';
-import { Card, Descriptions, message } from 'antd';
+import { Button, Card, Descriptions, Divider, Form, message } from 'antd';
+import TextArea from 'antd/es/input/TextArea';
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 
@@ -12,6 +16,10 @@ const Index: React.FC = () => {
   // 定义状态和钩子函数
   const [loading, setLoading] = useState(false);
   const [data, setData] = useState<API.InterfaceInfo>();
+  //存储结果变量
+  const [invokeRes, setInvokeRes] = useState<any>();
+  //调用加载状态变量，默认为false
+  const [invokeLoading, setInvokeLoading] = useState(false);
   // 使用 useParams 钩子函数获取动态路由参数
   const params = useParams();
 
@@ -41,6 +49,31 @@ const Index: React.FC = () => {
     loadData();
   }, []);
 
+  const onFinish = async (values: any) => {
+    // 检查是否存在接口id
+    if (!params.id) {
+      message.error('接口不存在');
+      return;
+    }
+    //调用前，设置为true，表示正在加载中
+    setInvokeLoading(true);
+    try {
+      // 发起接口调用请求，传入一个对象作为参数，这个对象包含了id和values的属性，
+      // 其中，id 是从 params 中获取的，而 values 是函数的参数
+      const res = await invokeInterfaceInfoUsingPost({
+        id: params.id,
+        ...values,
+      });
+      //将接口调用的结果（res.data）更新到invokeRes状态变量中
+      setInvokeRes(res.data);
+      message.success('请求成功');
+    } catch (error: any) {
+      message.error('操作失败，' + error.message);
+    }
+    //无论成功或失败，最后将invokeLoading设置为false,表示加载完成
+    setInvokeLoading(false);
+  };
+
   return (
     <PageContainer title="查看接口文档">
       <Card>
@@ -49,6 +82,7 @@ const Index: React.FC = () => {
             <Descriptions.Item label="接口状态">{data.status ? '开启' : '关闭'}</Descriptions.Item>
             <Descriptions.Item label="描述">{data.description}</Descriptions.Item>
             <Descriptions.Item label="请求地址">{data.url}</Descriptions.Item>
+            <Descriptions.Item label="请求参数">{data.requestParams}</Descriptions.Item>
             <Descriptions.Item label="请求方法">{data.method}</Descriptions.Item>
             <Descriptions.Item label="请求头">{data.requestHeader}</Descriptions.Item>
             <Descriptions.Item label="响应头">{data.responseHeader}</Descriptions.Item>
@@ -58,6 +92,24 @@ const Index: React.FC = () => {
         ) : (
           <>接口不存在</>
         )}
+      </Card>
+      <Divider>
+        <Card>
+          <Form name="invoke" onFinish={onFinish}>
+            <Form.Item label="请求参数" name="userRequestParams">
+              <TextArea />
+            </Form.Item>
+
+            <Form.Item wrapperCol={{ span: 16 }}>
+              <Button type="primary" htmlType="submit">
+                调用
+              </Button>
+            </Form.Item>
+          </Form>
+        </Card>
+      </Divider>
+      <Card title="返回结果" loading={invokeLoading}>
+        {invokeRes}
       </Card>
     </PageContainer>
   );
